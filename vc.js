778bc39ddb7b6e895d83de7b55c1a3b7a79f5917
Maks Nemisj <tableton@mail.ru>
2010-08-16 00:34:13 +0200

    Separating view from contoller

---
+++ vc.js
@@ -0,0 +1,119 @@
+window.view = {
+        nodes : {
+                timer : null,
+                timer_sec : null,
+                monitor : null,
+                code : null,
+                do_nothing_button : null
+        },
+        enable : function(bt) {
+                var node = this.nodes[bt];
+                if (node) {
+                        node.disabled = false;
+                }
+        },
+        disable : function(bt) {
+                var node = this.nodes[bt];
+                if (node) {
+                        node.disabled = true;
+                }
+        },
+        caption : function(bt,str) {
+                var node = this.nodes[bt];
+                if (node) {
+                        node.innerHTML = str;
+                }
+        },
+        value : function(node) {
+                var node = this.nodes[node];
+                return (node) ?  node.value : null;
+        }
+};
+
+window.controller = {
+    states : {},
+    state : function(sys, v) {
+        if (v == null) {
+            return this.states[sys];
+        } else {
+            this.states[sys] = ~~v;
+        }
+    },	
+
+    'swit4' : function(sys) {
+        var non = sys + "_switch";
+        var fc = this[non];
+        if (fc) {
+            this[non]();
+        }
+    },
+
+    monitor_switch : function() {
+        var is = this.state('monitor');
+        this.state( 'monitor', !is );
+
+        if (is) {
+            timer.stop();
+            progress.stop();
+
+            view.enable('timer');
+            view.caption('monitor','Start');
+            view.caption('timer','Start');
+        } else {
+            progress.collect();
+            timer.collect();
+
+            view.disable('timer');
+            view.caption('timer','Stop');
+            view.caption('monitor','Stop');
+        }
+    },
+
+    timer_switch : function() {
+
+        var is = this.state( 'timer' );
+        this.state( 'timer', !is );
+
+        if (is) {
+            view.caption( 'timer', 'Start' );
+            timer.stop();	
+        } else {
+            view.caption( 'timer', 'Stop' );
+
+            var sec = view.value('timer_sec');
+            if (!sec || isNaN( sec )) {
+                timer.collect();                                               
+            } else {
+                view.disable('timer');
+                timer.collect(Number(sec), function() {
+                    controller.timer_switch();
+                    view.enable( 'timer' );
+                });
+            }
+        }
+    },
+    
+    intercept : function() {
+        var code = view.value('code');
+        eval( code );
+    },
+
+    nothing : function() {
+        var is = this.state('nothing');
+        this.state('nothing', !is);
+        view.caption('nothing', is ? 'Do Nothing' : 'Do Something');
+    },
+
+	flashback_switch : function() {
+        var is = this.state('flashback');
+        this.state('flashback', !is);
+
+        if (is) {
+            // turn on timer
+            flashback.collect();
+        } else {
+            flashback.stop();
+            // turn of timer
+        }
+    }
+}
